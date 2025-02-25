b2171ea3a9830cf869293d365616782db84cf467
ajmarcelo <andreajeanmarcelo@gmail.com>
2025-02-26 03:02:26 +0800

    Code formatted with rubocop

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,7 @@ class Product < ApplicationRecord
     has_rich_text :description
     has_one_attached :featured_image
     has_many :subscribers, dependent: :destroy
-    
+
     validates :name, presence: true
     validates :inventory_count, numericality: { greater_than_or_equal_to: 0 }
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -12,7 +12,7 @@ class ProductTest < ActiveSupport::TestCase
 
         # Set product out of stock
         product.update(inventory_count: 0)
-        
+
         assert_emails 2 do
             product.update(inventory_count: 99)
         end
