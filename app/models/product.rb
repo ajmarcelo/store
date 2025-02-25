9cd4ecf236be0d6632e2fd58d454d53b9a481ebd
ajmarcelo <andreajeanmarcelo@gmail.com>
2025-02-26 02:13:21 +0800

    Added inventory count for product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ApplicationRecord
     has_rich_text :description
     has_one_attached :featured_image
+    
     validates :name, presence: true
+    validates :inventory_count, numericality: { greater_than_or_equal_to: 0 }
 end
